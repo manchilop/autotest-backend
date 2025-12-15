f3eabd25045c0f505f4b9dbbe803fa12ff91f8e5
manchilop <manuchicalopez@gmail.com>
2025-12-15 10:51:11 +0000

    chore: initialize backend with Spring Initializr

---
+++ src/main/java/com/example/autotest_backend/AutotestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.autotest_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutotestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutotestBackendApplication.class, args);
+	}
+
+}
